d6102640baab3cc5997d245dcc292b13eb125fe6
David Blot <david.blot9@gmail.com>
2022-11-25 10:31:50 +0100

    add router

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express'); // framework basé sur Node.JS
 const mongoose = require('mongoose'); // package qui facilite les interactions avec MongoDB
-
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
 
 // création de l'application avec express()
 const app = express();
@@ -40,51 +39,7 @@ app.use((req, res, next) => {
 	next();
 });
 
-// requête POST
-app.post('/api/stuff', (req, res, next) => {
-	delete req.body._id; // supprime car MongoDB en attribut déjà un.
-	const thing = new Thing({
-		// nouvelle instance du modele.
-		...req.body, // spread operator qui copie tous les champs du body.
-	});
-	thing
-		.save() // enregistre dans la base de données.
-		.then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-// Requête GET pour retourner tous les objets avec find()
-app.get('/api/stuff', (req, res, next) => {
-	Thing.find()
-		.then((things) => res.status(200).json(things))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-/*
-Requête GET pour retourner un seul objet avec findOne().
-La route inclut le paramètre dynamique de l'id avec /:id
-*/
-app.get('/api/stuff/:id', (req, res, next) => {
-	Thing.findOne({ _id: req.params.id })
-		.then((thing) => res.status(200).json(thing))
-		.catch((error) => res.status(404).json({ error }));
-});
-
-/*
-Requête PUT pour modifier un objet avec updateOne().
-La route reprend le parametre dynamique et on compare les id pour s'assurer que c'est bien l'objet voulu.
-*/
-app.put('/api/stuff/:id', (req, res, next) => {
-	Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-		.then(() => res.status(200).json({ message: 'Objet modifié !' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-// Requête DELETE pour supprimer un objet avec deleteOne().
-app.delete('/api/stuff/:id', (req, res, next) => {
-	Thing.deleteOne({ _id: req.params.id })
-		.then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-		.catch((error) => res.status(400).json({ error }));
-});
+// Pour la route '/api/stuff', on applique la logique du router.
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
